83189ed88cb1ccd8d74189cdbfa2a5414c1d95cb
miguelrsnavarrarioja-lab <miguelrsnavarrarioja@gmail.com>
2026-08-16 11:17:40 +0200

    Delete test_fb.js

--- test_fb.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-// This is frontend code, I can't query firebase from here easily because I'm on a local machine without firebase admin set up, or am I?
-console.log("I can't run this easily")
